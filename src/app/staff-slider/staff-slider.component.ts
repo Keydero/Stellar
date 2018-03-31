af1f0a6a67c21e4efffc5d079d63c896fe1ce4c8
Oussama Keddar <oussama.keddar@epitech.eu>
2018-03-31 19:06:53 +0200

    icon add set, fill doesn't work, so i changed the value within the svg file

--- src/app/staff-slider/staff-slider.component.ts
+++ src/app/staff-slider/staff-slider.component.ts
@@ -9,14 +9,14 @@ export class StaffSliderComponent implements OnInit {
  	items  = [];
   constructor() { 
   	this.items = [
-{ name: 'Oussama Keddar', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.png'  },
-{ name: 'Oussama Keddar', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.png'  },
-{ name: 'Oussama Keddar', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.png'  },
+{ name: 'Oussama Keddar', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.jpg'  },
+{ name: 'Jhon Doe', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.jpg'  },
+{ name: 'Doe Jhon', job: 'Full-stack Developer', body: 'Oussama is a very skilled. He used to work for no one. He puts passion in every project and creates amazing interfaces', avatar: '/assets/oussama.jpg'  },
   	];
   }
 
   ngOnInit() {
-  	// console.log(this.items);
+
   }
 
 }
